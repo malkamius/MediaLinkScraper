fb65c218f26ec7c839b23835e748ef18f5bd3842
malkamius <cking999@gmail.com>
2023-12-12 15:25:44 -0600

    Update content.js
    Updated content to use chrome local storage to communicate with popup

--- src/content.js
+++ src/content.js
@@ -1,8 +1,25 @@
 // Search for video elements or other relevant tags
 const videos = document.querySelectorAll('video');
 videos.forEach(video => {
-    if (video.src.includes('.m3u8') || video.src.includes('.mp4')) {
+    if (video.src.toLowerCase().includes('.m3u8') || video.src.toLowerCase().includes('.mp4')) {
         // Send the video source URL to the background script
-        chrome.runtime.sendMessage({url: video.src});
+		chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+			if (tabs.length === 0) return; // No active tab found
+
+			const currentTabId = tabs[0].id;
+			chrome.storage.local.get({mediaLinks: {}}, function(result) {
+				let updatedLinks = result.mediaLinks;
+				
+
+				if (!updatedLinks[currentTabId]) {
+					updatedLinks[currentTabId] = [];
+				}
+
+				if (!updatedLinks[currentTabId].includes(details.url)) {
+					updatedLinks[currentTabId].push(details.url);
+					chrome.storage.local.set({mediaLinks: updatedLinks});
+				}
+			});
+		});
     }
 });
